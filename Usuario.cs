d0c63674339e5cbdfe93b540544b3bc29ec6a170
Paola Ponce <83021036+paobit@users.noreply.github.com>
2022-03-15 21:47:46 +0100

    Add files via upload

---
+++ Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace repaso_1
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+
+        public string Nombre { get;set; }
+        
+        public string Passeword { get; set; }
+
+        public string Email { get; set; }
+
+        public string post { get; set; }
+
+        public string Comments { get; set; }
+
+    }
+
+   
+}
