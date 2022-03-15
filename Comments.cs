05ceb8bede7d9aa815a7c1096f1e1189a5c2a523
Paola Ponce <83021036+paobit@users.noreply.github.com>
2022-03-15 21:44:23 +0100

    Add files via upload

---
+++ Comments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace repaso_1
+{
+    public class Comments:Usuario
+    {
+        public int ID { get; set; }
+        public string Date { get; set; }
+        public string Comment { get; set; }
+        public Usuario Usuario{ get; set; }
+
+    }
+}
