88573c056a205b17af9105a6c86c7c55adf1bef3
Paola Ponce <83021036+paobit@users.noreply.github.com>
2022-03-15 21:47:09 +0100

    Add files via upload

---
+++ Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace repaso_1
+{
+    public class Posts
+    {
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public string Content { get; set; }
+        public string Post { get; set; }
+
+    
+    }
+}
